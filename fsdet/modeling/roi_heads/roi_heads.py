c7e97064cfc2559f49fcc52e9cba910a720d7468
Werner Bailer <werner.bailer@joanneum.at>
2022-12-22 15:57:19 +0100

    fixed first_stride issue for compatibility with newer Detectron versions

--- fsdet/modeling/roi_heads/roi_heads.py
+++ fsdet/modeling/roi_heads/roi_heads.py
@@ -318,10 +318,13 @@ class Res5ROIHeads(ROIHeads):
             "Deformable conv is not yet supported in res5 head."
         # fmt: on
 
+        # first_stride: fix for newer versions of detectron
+
         blocks = make_stage(
             BottleneckBlock,
             3,
-            first_stride=2,
+            #first_stride=2,
+            stride_per_block=[2, 1, 1],
             in_channels=out_channels // 2,
             bottleneck_channels=bottleneck_channels,
             out_channels=out_channels,
